8c5e4c7efeda85efea96db94b800ab0eee7c90fc
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-01 16:48:12 -0500

    feat: config reading, saving

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ctx
 
 manage project research, tasks, and other things at the command line
+
+## Roadmap
+
++ launch browser tabs based on git repo
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,68 @@
 // std
+use std::collections::HashMap;
+use std::fs::{create_dir, read_to_string, write};
+use std::path::PathBuf;
 
 // external
+use directories::BaseDirs;
 use serde::{Deserialize, Serialize};
+use serde_json::{from_str, to_string_pretty};
+
+// local
+
+fn scaffold() -> std::io::Result<()> {
+    let bd = BaseDirs::new().expect("Failed to read configuration");
+    let config_directory = bd.config_dir().join("ctx/");
+    create_dir(config_directory.clone())?;
+    Ok(write(
+        config_directory.join("config.json"),
+        "{ \"workspaces\": {} }",
+    )?)
+}
 
 #[derive(Serialize, Deserialize)]
+/// ctx's configuration, as read from the user's configuration file located in their configuration directory
 pub struct Config {
-    workspaces: Vec<String>,
+    /// a list of all workspaces and the file system paths to them
+    pub workspaces: HashMap<String, PathBuf>,
 }
 
 impl Config {
     pub fn get() -> Config {
-        Config {
-            workspaces: Vec::new(),
+        let bd = BaseDirs::new().expect("Failed to find config directory.");
+        let config_directory = bd.config_dir().join("ctx");
+        if config_directory.is_dir() == false {
+            scaffold().expect("Failed to scaffold configuration.");
         }
+        let config: Config = from_str(
+            read_to_string(config_directory.join("config.json"))
+                .expect("Failed to read config file!")
+                .as_str(),
+        )
+        .expect("Failed to serialise configuration.");
+
+        config
+    }
+
+    /// adds a workspace to the current configuration instance, which is then saved to the configuration file
+    pub fn add_workspace<T: Into<String>>(
+        &mut self,
+        workspace: T,
+        path: PathBuf,
+    ) -> std::io::Result<()> {
+        self.workspaces.insert(workspace.into(), path);
+        self.save()
+    }
+
+    /// saves the deserialised self instance into the configuration file
+    pub fn save(&self) -> std::io::Result<()> {
+        let bd = BaseDirs::new().expect("Failed to find config directory.");
+        let config_directory = bd.config_dir().join("ctx");
+        write(
+            config_directory.join("config.json"),
+            to_string_pretty(self).expect("Failed to save configuration"),
+        )
     }
 }
+
+pub struct Project {}
